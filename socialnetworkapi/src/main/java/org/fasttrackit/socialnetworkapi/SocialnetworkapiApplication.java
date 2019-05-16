3735b1f8e8793486dc7e277bb0095c902ccb3dae
Ahmad09Bakour <ahmad.hajbakour@gmail.com>
2019-05-16 19:34:30 +0300

    First step

---
+++ socialnetworkapi/src/main/java/org/fasttrackit/socialnetworkapi/SocialnetworkapiApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.socialnetworkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialnetworkapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialnetworkapiApplication.class, args);
+	}
+
+}
